c494692fd79d2308db8e4e22d4c4013a5b72d233
Daniel Toll <daniel_toll@hotmail.com>
2019-09-19 07:01:42 +0200

    A late first commit

--- controllers/image.js
+++ controllers/image.js
@@ -2,13 +2,14 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: 'ed3199354ab84bb0828ed7c988eadab1'
 });
 
 const handleApiCall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
+      console.log(' api data: ', data);
       res.json(data);
     })
     .catch(err => res.status(400).json('unable to work with API'))
