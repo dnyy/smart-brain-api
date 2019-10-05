21ade71833ec175b5ba414c6c793a7c39a2ecf4f
Daniel Toll <daniel_toll@hotmail.com>
2019-10-05 19:15:19 +0200

    add age and pet to db and hanlde updates

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (name, email, entries, joined ) values ('devvy', 'devvy@gmail.com', 5, '2019-01-01');
+INSERT INTO users (name, age, pet, email, entries, joined ) values ('devvy', 18, 'dog', 'devvy@gmail.com', 5, '2019-01-01');
 INSERT INTO login (hash, email) values ('$2a$10$g2yrxJMo2tEQvcqImuSQtObQW0rXAqkA3JidD5AZpTmyEP8Azvda.', 'devvy@gmail.com');
 
 COMMIT;
