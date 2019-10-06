b6c265255008e16263d9ec9f4a3a887a962f1755
Daniel Toll <daniel_toll@hotmail.com>
2019-10-06 15:38:55 +0200

    handle token setup on register endpoint

--- controllers/register.js
+++ controllers/register.js
@@ -1,10 +1,18 @@
-const handleRegister = (req, res, db, bcrypt) => {
+const jwt = require('jsonwebtoken');
+const redis = require('redis');
+
+const redisClient = require('./signin').redisClient;
+
+// Setup Redis:
+// const redisClient = redis.createClient(process.env.REDIS_URI);
+
+const handleRegister = (db, bcrypt, req, res) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
+    return db.transaction(trx => {
       trx.insert({
         hash: hash,
         email: email
@@ -29,8 +37,50 @@ const handleRegister = (req, res, db, bcrypt) => {
     .catch(err => res.status(400).json('unable to register'))
 }
 
+const getAuthTokedId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    
+    if (err || !reply) {      
+      return res.status(400).json('Unauthorized');
+    }
+    return res.json({ id: reply });
+  })
+}
+
+const signToken = (email) => {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' }); // use env variable
+
+}
+
+const setToken = (key, value) => {
+  return Promise.resolve(redisClient.set(key, value))
+}
+
+const createSessions = (user) => {
+  // create jwt, return user data
+  const { email, id } = user;
+  const token = signToken(email);
+  return setToken(token, id)
+    .then(() => ({ success: 'true', userId: id, token }))
+    .catch(console.log)
+
+}
+const registerAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization ? getAuthTokedId(req, res) :
+    handleRegister(db, bcrypt, req, res)
+      .then(data => {
+        return data.id && data.email ? createSessions(data) : Promise.reject(data)
+      })
+      .then(session => res.json(session))
+      .catch(err => res.status(400).json(err))
+};
+
 module.exports = {
-  handleRegister: handleRegister
+  registerAuthentication: registerAuthentication,
+  redisClient: redisClient,
 };
 
 
